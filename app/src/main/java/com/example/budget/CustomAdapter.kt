6cc7e1f1bcfca6a6f33a1b9bcd12defb7a974f86
Joann S. <303940abc@gmail.com>
2024-05-04 23:42:02 -0700

    All connected now.

---
+++ app/src/main/java/com/example/budget/CustomAdapter.kt
@@ -0,0 +1,5 @@
+package com.example.budget
+
+class CustomAdapter(mainActivity2: MainActivity2, titleList: ArrayList<String>, detailList: ArrayList<String>, dateList: ArrayList<String>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+}
